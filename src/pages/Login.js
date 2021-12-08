f42a8ac2453e5ed988b5f700978e226c3846742a
gabriela-veloso <gabrielaveloso.engmateriais@gmail.com>
2021-12-08 17:34:34 -0300

    Requisito Login em andamento

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import RecipesContext from '../contexts/RecipesContext';
 
 function Login() {
+  const {
+    handleEmail,
+    handlePassword,
+    email,
+    password,
+    disabled,
+  } = useContext(RecipesContext);
+
   return (
     <div>
       <label htmlFor="email">
-        <input type="email" data-testid="email-input" />
+        <input
+          type="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ handleEmail }
+        />
       </label>
       <label htmlFor="password">
-        <input type="password" data-testid="password-input" />
+        <input
+          type="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ handlePassword }
+        />
       </label>
-      <button type="button" data-testid="login-submit-btn">Enviar</button>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ disabled }
+        onClick={ handleSubmit }
+      >
+        Enviar
+      </button>
     </div>
   );
 }
