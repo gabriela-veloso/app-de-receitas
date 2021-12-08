469053845deedb0a3469d7f4a9db4cf8e4108fa0
gabriela-veloso <gabrielaveloso.engmateriais@gmail.com>
2021-12-08 18:45:03 -0300

    Requisito 13

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,6 +32,37 @@ export default function Header({ title, showSearchIcon = true }) {
       }
       { showSearchIcon && showSearchInput
       && <input type="text" data-testid="search-input" />}
+      <label htmlFor="ingredient">
+        Ingrediente
+        <input
+          type="radio"
+          id="ingredient"
+          name="filter-radio"
+          value="ingredient"
+          data-testid="ingredient-search-radio"
+        />
+      </label>
+      <label htmlFor="name">
+        Nome
+        <input
+          type="radio"
+          id="name"
+          name="filter-radio"
+          value="name"
+          data-testid="name-search-radio"
+        />
+      </label>
+      <label htmlFor="first-letter">
+        Primeira Letra
+        <input
+          type="radio"
+          id="first-letter"
+          name="filter-radio"
+          value="first-letter"
+          data-testid="first-letter-search-radio"
+        />
+      </label>
+      <button type="button" data-testid="exec-search-btn">Buscar</button>
     </div>
   );
 }
