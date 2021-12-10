aeeb04b42ee1bfa1bde54f5eb9773cba85f7665d
gabriela-veloso <gabrielaveloso.engmateriais@gmail.com>
2021-12-10 15:27:33 -0300

    Requisito 25 e 26

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,17 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import RecipesContext from '../contexts/RecipesContext';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { fetchFoodOnLoad } from '../services/comidasApi';
 
 export default function MainPage() {
   const { mealsData } = useContext(RecipesContext);
-  console.log(mealsData);
+  const [meals, setMeals] = useState(mealsData);
   const MAX_NUM = 12;
+
+  useEffect(() => {
+    async function handleMealsOnLoad() {
+      if (mealsData.length === 0) {
+        const food = await fetchFoodOnLoad();
+        setMeals(food);
+      }
+    }
+    handleMealsOnLoad();
+  }, [mealsData]);
 
   return (
     <div>
       <Header title="Comidas" />
-      { mealsData && mealsData.slice(0, MAX_NUM).map((elem, index) => (
+      { meals && meals.slice(0, MAX_NUM).map((elem, index) => (
         <div
           data-testid={ `${index}-recipe-card` }
           key={ elem.idMeal }
--- src/services/comidasApi.js
+++ src/services/comidasApi.js
@@ -56,3 +56,9 @@ export async function fetchRandomMeal() {
   const json = await response.json();
   return json.meals;
 }
+
+export async function fetchFoodOnLoad() {
+  const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+  const json = await response.json();
+  return json.meals;
+}
